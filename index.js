a70773619d249b6b75d81638bdd341f8c19ecde4
patriciaviertler <1910738097@stud.fh-kufstein.ac.at>
2020-06-23 19:31:32 +0200

    final changes

--- index.js
+++ index.js
@@ -1,60 +1,76 @@
-const http2 = require('http2');
-const fs = require('fs');
-const path = require('path');
-
-const options = {
-    key: fs.readFileSync('privateKey.key'),
-    cert: fs.readFileSync('certificate.crt')
-}
-
-const server = http2.createSecureServer(options);
-
-/*server.on('stream', (stream, headers) => {
-    console.log(stream);
-    fs.readFile(__dirname + '/www/' + headers[':path'], (err, data) => {
-        console.log(data)
-    });
-    stream.end("Hello World");
-})
-
-server.listen(8080, () => console.log("Server is running."))*/
-
-server.on('request', (req, res) => {
-    console.log(path.resolve(__dirname, 'www', 'index.html'));
-    fs.readFile(__dirname + req.url, function (err, data) {
-        if (err) {
-            res.writeHead(404);
-            res.end(JSON.stringify(err));
-            return;
-        }
-        res.writeHead(200);
-        res.end(data);
-    })
-})
-
-/*
-//imagemin, um mehrere Bilder zu minimieren
-
 const imagemin = require('imagemin');
 const imageminJpegoptim = require('imagemin-jpegoptim');
 const imageminWebp = require('imagemin-webp');
+const critical = require('critical');
+const minify = require('minify');
+const fs = require('fs');
+
 
 (async () => {
     const files = await imagemin(['./www/images/*.{jpg,png}'], {
         destination: './www/images',
         plugins: [
-            //imageminJpegoptim({ progressive: true, max: 80 }),
-            imageminWebp({ quality: 100, size: 20000, resize: { width: 800, height: 450 } })
-
-        ]
+            imageminWebp({ quality: 100, size: 20000, resize: { width: 800, height: 450 } }),
+        ],
     });
 
     const filesBackground = await imagemin(['./www/images/background/*.{jpg,png}'], {
         destination: './www/images/background',
         plugins: [
-            //imageminJpegoptim({ progressive: true, max: 80 }),
             imageminWebp({ quality: 100, size: 200000 })
 
         ]
     });
-})();*/
+})();
+
+const options = {
+    html: {
+        removeAttributeQuotes: false,
+        removeOptionalTags: false,
+    },
+};
+
+(async () => {
+    try {
+        const data = await minify('./www/css/style.css', options);
+        fs.writeFile('./www/css/style.mini.css', data, (err) => {
+            if (err) throw err;
+            console.log('the file has been saved!');
+        });
+    } catch (err) {
+        console.log(err.message);
+    }
+})();
+
+fs.readdir('./www/js/', async (err, files) => {
+    console.log(files);
+    for (let file in files) {
+        if (!files[file].includes('min.js')) {
+            try {
+                const data = await minify(`./www/js/${files[file]}`, options);
+                const fileSplit = files[file].split('.');
+                const fileNameFragmentations = fileSplit.slice(0, -1);
+                const fileName = fileNameFragmentations.join('.');
+
+
+                fs.writeFile(`./www/js/${fileName}.min.js`, data, (err) => {
+                    if (err) throw err;
+                    console.log('The file has been saved!');
+                });
+            } catch (err) {
+                console.log(err);
+            }
+        }
+    }
+});
+
+critical.generate({
+    inline: true,
+    base: 'www/',
+    src: 'index-source.html',
+    dest: 'index.html',
+    width: 500,
+    height: 800,
+    minify: true,
+});
+
